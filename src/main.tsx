a2d7af815c6403e8da7b8c2887b7057921afc1ce
jprkindrid <jpriveraa7@gmail.com>
2025-12-22 23:48:20 -0600

    refactor: preemtively added routes so they were removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./routes.tsx";
+import App from "./App";
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -10,7 +9,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
+            <App />
         </QueryClientProvider>
     </StrictMode>
 );
